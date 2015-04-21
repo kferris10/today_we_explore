9701ba50e2204f1fac85a6061f133057bdd6bccd
kferris10 <kevin.ferris10@gmail.com>
2015-04-21 13:07:04 -0600

    Initial commit

---
+++ leaflet-route.R
@@ -0,0 +1,27 @@
+
+library(plotKML)
+library(leaflet)
+
+setwd("~/temp/keenan_bike")
+k_tracks <- readGPX("Today, we explore.gpx")
+
+library(listviewer)
+jsonedit(k_tracks)
+y <- k_tracks$tracks[[1]][[1]]
+write.csv(subset(y, select = c(lat, lon)), 
+          file = "keenan_gps.csv", quote = F, row.names = F)
+
+library(rgdal)
+dataMap.SP  <- SpatialPointsDataFrame(subset(y, select = c(lat, lon)), y)
+str(dataMap.SP) # Now is class SpatialPointsDataFrame
+
+#Write as geojson
+writeOGR(dataMap.SP, 'keenan_bike.geojson','dataMap', driver='GeoJSON')
+
+p1 <- leaflet() %>% 
+  addTiles() %>% 
+  setView(lng = -112.0307, lat = 46.57573, zoom = 13) %>% 
+  addPolylines(lng = y$lon, lat = y$lat)
+p1
+
+jsonedit(p1)
